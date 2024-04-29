e6f16252c6769681c780bd25a1f3c48a9d625516
Silent <mikolaj@statystyka.net>
2024-04-29 15:37:30 +0200

    Improved movement system, started working on pixelate shader

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -5,11 +5,11 @@ extends RigidBody2D
 
 @export_category("Movement variables")
 @export_group("Horizontal movement")
-#@export var max_speed: float = 400.0
-@export var movement_force: float = 2500.0
+@export var max_speed: float = 500.0
+@export var movement_force: float = 1700.0
 @export var air_movement_force: float = 500.0
 
-var floor_objects: Array[PhysicsBody2D] = []
+var floor_friction: float = 0.0
 var is_on_floor = false
 
 @export_group("Vertical movement")
@@ -18,21 +18,26 @@ var is_on_floor = false
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity") # get gravity value from settings
 const STEP_FACTOR = 250.0
 
+var default_friciton
+
 func _ready():
+	default_friciton = physics_material_override.friction
 	movement_force *= mass
 	air_movement_force *= mass
 	jump_force *= mass
 
 func get_floor(state: PhysicsDirectBodyState2D):
-	floor_objects = []
+	floor_friction = 0.0
 	is_on_floor = false
 	for contact_index in state.get_contact_count():
 		var collision_normal = state.get_contact_local_normal(contact_index)
 		var body = state.get_contact_collider_object(contact_index)
 		if collision_normal.dot(Vector2(0, -1)) > 0.6:
-			floor_objects.append(body)
-	if floor_objects.size() > 0:
-		is_on_floor = true
+			is_on_floor = true
+			if body.physics_material_override:
+				floor_friction = max(floor_friction, body.physics_material_override.friction)
+			else:
+				floor_friction = PhysicsMaterial.new().friction # default friction
 
 #func apply_push_forces():
 	#for i in get_slide_collision_count():
@@ -44,27 +49,30 @@ func get_floor(state: PhysicsDirectBodyState2D):
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	get_floor(state)
 	var step = state.step * STEP_FACTOR
+	var velocity = state.linear_velocity
 #
 	if Input.is_action_pressed("Right"):
-		if is_on_floor:
-			apply_central_force(Vector2.RIGHT * movement_force * step)
-		else:
-			apply_central_force(Vector2.RIGHT * air_movement_force * step)
+		if velocity.x < max_speed:
+			if is_on_floor:
+				apply_central_force(Vector2.RIGHT * movement_force * step)
+				physics_material_override.friction = 0.0
+			else:
+				apply_central_force(Vector2.RIGHT * air_movement_force * step)
 		$Sprite.flip_v = false
 	elif Input.is_action_pressed("Left"):
-		if is_on_floor:
-			apply_central_force(Vector2.LEFT * movement_force * step)
-		else:
-			apply_central_force(Vector2.LEFT * air_movement_force * step)
+		if velocity.x > -max_speed:
+			if is_on_floor:
+				apply_central_force(Vector2.LEFT * movement_force * step)
+				physics_material_override.friction = 0.0
+			else:
+				apply_central_force(Vector2.LEFT * air_movement_force * step)
 		$Sprite.flip_v = true
-
-	##velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	else:
+		physics_material_override.friction = default_friciton
 	
 	if Input.is_action_just_pressed("Up"):
 		apply_central_impulse(Vector2.UP * jump_force)
-		
-	#apply_push_forces()
-	#state.linear_velocity = velocity
+	
 	state.angular_velocity = 0.0 # lock rotation
 	rotation = 0.0
 
